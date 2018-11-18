e721a4a49ef52362ed8281c72324e6edcd0b6853
MiroG <miroslaw.golda@gmail.com>
2018-11-18 11:38:25 +0100

    templates

--- src/main/java/com/example/bookstorefrontend/controller/BookController.java
+++ src/main/java/com/example/bookstorefrontend/controller/BookController.java
@@ -1,12 +1,12 @@
 package com.example.bookstorefrontend.controller;
 
+import com.example.bookstorefrontend.model.BookForm;
 import com.example.bookstorefrontend.service.BookApiClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping
@@ -22,6 +22,36 @@ public class BookController {
     @GetMapping
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/about")
+    public String about() {
+        return "books/about";
+    }
+
+    @GetMapping("/books")
+    public String listing(Model model) {
+        model.addAttribute("listing", client.fetchAllBooks());
+        return "books/listing";
+    }
+
+    @GetMapping("/books/add")
+    public String addPage() {
+        return "books/add";
+    }
+
+    @PostMapping("/books/add")
+    public String add(@ModelAttribute BookForm bookForm,
+                      RedirectAttributes redirectAttributes) {
+        //TODO metoda klienta na dodawanie
+        redirectAttributes.addFlashAttribute("result", "Książka została dodana.");
+        return "redirect:/books";
+    }
+
+    @GetMapping("/books/{bookId}")
+    public String details(@PathVariable String bookId, Model model) {
+        model.addAttribute("book", client.fetchBookInfo(bookId));
+        return "books/details";
     }
 
     @GetMapping("/showme")
